7215182176c8ff6f58b75cb7a75b1a7f4f36c618
arindom7 <arindom707@gmail.com>
2026-09-04 19:47:53 +0530

    done

--- front-end/components/index.ts
+++ front-end/components/index.ts
@@ -2,11 +2,8 @@ export * from './app-header';
 export * from './get-started-button';
 export * from './hamburger-button';
 export * from './notification-button';
-<<<<<<< HEAD
 export * from './notification-context';
 export * from './notification-panel';
 export * from './role-card';
-=======
->>>>>>> dbbc990dbdaad075c587cc2194bee2672c08d3fc
 export * from './side-menu';
 export * from './side-menu-context';
